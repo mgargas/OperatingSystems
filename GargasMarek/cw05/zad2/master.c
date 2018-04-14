6420425757ca43282e3ccf165916eec57a17b6c3
Marek Gargas <250497@wp.pl>
2018-04-15 00:25:49 +0200

    Everything works perfect

--- GargasMarek/cw05/zad2/master.c
+++ GargasMarek/cw05/zad2/master.c
@@ -10,11 +10,22 @@
 #define LINE_MAX 256
 
 int main(int argc, char **argv) {
-    mkfifo("", S_IWUSR | S_IRUSR);
-    FILE* pipe = fopen("./kk", "r");
-    char buffer[LINE_MAX];
+    if(argc != 2) {
+        printf("Wrong arguments.\nusage:\nmaster path\n");
+        exit(1);
+    }
+    if(mkfifo(argv[1], S_IWUSR | S_IRUSR)){
+        printf("Error while creating fifo.\n");
+        exit(1);
+    }
+    FILE* pipe = fopen(argv[1], "r");
+    if(pipe == NULL){
+        printf("Error while opening file.\n");
+        exit(1);
+    }
+    char buffer[LINE_MAX+1];
 
-    while (fgets(buffer, LINE_MAX, pipe) != NULL) {
+    while (fgets(buffer, LINE_MAX+1, pipe) != NULL) {
         write(1, buffer, strlen(buffer));
     }
     fclose(pipe);
--- GargasMarek/cw05/zad2/slave.c
+++ GargasMarek/cw05/zad2/slave.c
@@ -2,12 +2,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <memory.h>
 
 #define LINE_MAX 256
 
+int is_number(char* arg) {
+    for(int i = 0; i < strlen(arg); i++)
+        if(arg[i]<'0' || arg[i]>'9') return 0;
+    return 1;
+}
+
 int main(int argc, char** argv) {
-    int pipe = open("./kk", O_WRONLY);
-    write(pipe, "komunikat", LINE_MAX);
+    if(argc != 3) {
+        printf("Wrong arguments.\nusage:\nslave path N\n");
+        exit(1);
+    }
+    if(!is_number(argv[2])) {
+        printf("Wrong arguments.\nN has to be a natural number.\n");
+        exit(1);
+    }
+    int N = atoi(argv[2]);
+    int pipe = open(argv[1], O_WRONLY);
+    if(pipe == -1) {
+        printf("Error while opening file.\n");
+        exit(1);
+    }
+    char sys_date[LINE_MAX];
+    char message[LINE_MAX];
+    FILE* date;
+    for(int i=0;i<N;i++){
+        date = popen("date", "r");
+        if(date == NULL){
+            printf("Error while opening file with date.\n");
+            exit(1);
+        }
+        fgets(sys_date, LINE_MAX, date);
+        sprintf(message, "Slave: %d - %s", getpid(), sys_date);
+        write(pipe, message, strlen(message));
+        fclose(date);
+        sleep(4);
+    }
     close(pipe);
     return 0;
 }
